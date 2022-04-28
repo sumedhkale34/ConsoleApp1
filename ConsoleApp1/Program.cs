0d36634718e52dd2d75327dacc9afec1b24594f6
sumedhkale34 <sumedh.kale34@gmail.com>
2022-04-28 17:17:02 +0530

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Sumedh");
+            int a = 10;
+            Console.WriteLine(a);
         }
     }
 }
